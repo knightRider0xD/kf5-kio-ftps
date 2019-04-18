1ac5e07316eb79bce5592bb88b6a9817aff8fb23
Ian <ian@knightly.xyz>
2019-04-18 14:16:37 +0930

    default port

--- ftp.cpp
+++ ftp.cpp
@@ -181,7 +181,7 @@ Ftp::Ftp( const QByteArray &pool, const QByteArray &app )
   ftpCloseControlConnection();
 
   // init other members
-  m_port = 0;
+  m_port = 990;
   qCDebug(KIO_FTPS) << "Ftp::Ftp()";
 }
 
